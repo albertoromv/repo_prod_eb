46bd45d280786d1e35e92c197d35448967e51d1f
Alberto <albertoromv@hotmail.com>
2023-05-06 11:45:29 +0200

    update
    update

---
+++ application.py
@@ -0,0 +1,34 @@
+from flask import Flask, jsonify
+import pickle
+
+model = pickle.load(open('iri.pkl', 'rb'))
+
+application = Flask(__name__)
+
+@application.route('/', methods=['GET'])
+def home():
+    return "Hello world!"
+
+
+@application.route('/api/data1/<int:data1>/data2/<int:data2>/data3/<int:data3>/data4/<int:data4>', methods=['GET'])
+def predict2(data1, data2, data3, data4):
+    prediction = model.predict([[data1, data2, data3, data4]])
+
+    if prediction[0] == 0:
+        name = "Setosa"
+    else:
+        name = "Versicolor"
+
+    response = {
+        'prediction': {
+            'type': str(prediction[0]),
+            'name': name
+        }
+    }
+
+    print(prediction[0])
+    return jsonify(response)
+
+
+if __name__ == "__main__":
+    application.run(debug=True)
